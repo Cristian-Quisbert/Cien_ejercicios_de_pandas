5389556defcf99f7f2d8c2b84078371e57c746fe
Cristian <CristianQuisbert@proton.me>
2026-05-16 22:09:35 -0400

    Repositorio de prueba

---
+++ 1.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio 1: Creación de un DataFrame y exploración inicial
+
+Enunciado: Crea un DataFrame llamado df_acciones que contenga la siguiente información sobre tres empresas (Apple, Microsoft y Google):
+
+Una columna llamada Ticker con los valores: 'AAPL', 'MSFT', 'GOOG'.
+
+Una columna llamada Precio con los valores: 175.50, 420.20, 150.10.
+
+Una columna llamada Volumen con los valores: 50000000, 23000000, 28000000.
+
+Una vez creado, muestra en pantalla solo las 2 primeras filas del DataFrame.
+"""
+
